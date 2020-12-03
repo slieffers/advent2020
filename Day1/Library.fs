6cd2545421c036a86826b9a3e1ed1883c6e10a77
Shane Lieffers <TuthmosisIII@msn.com>
2020-12-02 23:10:28 -0500

    Day 1

---
+++ Day1/Library.fs
@@ -0,0 +1,35 @@
+﻿namespace Day1
+
+module Code =
+    let rec findNums (total: int) (dict: Map<int, int>) (nums: list<int>) =
+        if dict.ContainsKey(nums.Head) 
+        then 
+            Some (nums.Head, dict.[nums.Head])
+        elif nums.Length = 1
+        then
+            None
+        else
+            findNums total (dict.Add(total - nums.Head, nums.Head)) nums.Tail
+
+    let combineTuples (dict: Map<int, int>) tup =
+        (fst tup, snd tup, dict.[(fst tup) + (snd tup)])
+
+    let part1 nums = 
+        nums 
+        |> Seq.toList 
+        |> findNums 2020 Map.empty
+        |> Option.get
+
+    let part2 nums = 
+        let dict1 = 
+            nums 
+            |> Seq.toList 
+            |> List.map(fun i -> 2020 - i, i) 
+            |> Map.ofList
+
+        dict1
+        |> Map.toList
+        |> List.map fst
+        |> List.tryPick(fun k -> findNums k Map.empty (nums |> Seq.toList))
+        |> Option.get
+        |> combineTuples dict1
