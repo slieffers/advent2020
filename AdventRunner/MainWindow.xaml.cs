c8a813470dd15445ac7324b90e61c9db054c5ab3
Shane Lieffers <TuthmosisIII@msn.com>
2020-12-02 23:10:28 -0500

    Day 1

---
+++ AdventRunner/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Day1;
+
+namespace AdventRunner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextReader tr = new StreamReader(@"Day1.txt");
+            IEnumerable<int> numbers = tr.ReadToEnd().Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            var myTextBox = FindChild<TextBox>(Application.Current.MainWindow, "MainTextBox");
+
+            (int part1Num1, int part1Num2) = Code.part1(numbers);
+            (int part2Num1, int part2Num2, int part2Num3) = Code.part2(numbers);
+
+            myTextBox.Text = $"Part 1: {part1Num1 * part1Num2}\r\nPart2: {part2Num1 * part2Num2 * part2Num3}";
+        }
+
+        public static T FindChild<T>(DependencyObject parent, string childName)
+            where T : DependencyObject
+        {
+            if (parent == null)
+            {
+                return null;
+            }
+
+            T foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (var i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                if (!(child is T childType))
+                {
+                    foundChild = FindChild<T>(child, childName);
+
+                    if (foundChild != null) break;
+                }
+                else if (!string.IsNullOrEmpty(childName))
+                {
+                    if (child is FrameworkElement frameworkElement && frameworkElement.Name == childName)
+                    {
+                        foundChild = (T)child;
+                        break;
+                    }
+                }
+                else
+                {
+                    foundChild = (T)child;
+                    break;
+                }
+            }
+
+            return foundChild;
+        }
+    }
+}
