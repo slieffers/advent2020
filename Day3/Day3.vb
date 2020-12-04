92fafaf3effdfe68891ad67959383f3e67173388
Shane Lieffers <TuthmosisIII@msn.com>
2020-12-03 20:22:42 -0500

    Day 3

--- Day3/Day3.vb
+++ Day3/Day3.vb
@@ -3,22 +3,12 @@ Namespace Vb.Code
         Public Shared Function Part1(forest as List(Of String)) As Int32
             Return Traverse(forest, 3, 1)
         End Function
-        
+
         Public Shared Function Part2(forest as List(Of String)) As Int32
-            Dim slopes = New List(Of ValueTuple(Of Int32, Int32))
-            slopes.Add((1,1))
-            slopes.Add((3,1))
-            slopes.Add((5,1))
-            slopes.Add((7,1))
-            slopes.Add((1,2))
-            
-            Dim total = 1
-            For Each slope As ValueTuple(Of Int32, Int32) In slopes
-                total *= Traverse(forest, slope.Item1, slope.Item2)
-            Next
-            Return total
+            Dim slopes = New List(Of ValueTuple(Of Int32, Int32)) From {(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)}
+            Return slopes.Aggregate(1, Function(current, slope) current * Traverse(forest, slope.Item1, slope.Item2))
         End Function
-        
+
         Private Shared Function Traverse(forest as List(Of String), horizontalMove As Int32, verticalMove As Int32) As Int32
             Dim treeCount = 0
             Dim column = horizontalMove
